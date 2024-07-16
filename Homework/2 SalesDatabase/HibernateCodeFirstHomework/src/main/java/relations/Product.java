e1cd4832310765409ec5309ae397be7efd935fda
Adrian9923 <114826782+Adrian9923@users.noreply.github.com>
2024-07-16 18:55:27 +0300

    Add files via upload

---
+++ Homework/2 SalesDatabase/HibernateCodeFirstHomework/src/main/java/relations/Product.java
@@ -0,0 +1,57 @@
+package relations;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+@Entity
+@Table(name = "products")
+public class Product extends BaseEntity{
+    private String name;
+    private double quantity;
+    private BigDecimal price;
+    private Set<Sale> sales;
+
+    public Product() {
+    }
+
+    @Column
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    @Column
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @OneToMany(mappedBy = "product")
+    public Set<Sale> getSales() {
+        return sales;
+    }
+
+    public void setSales(Set<Sale> sales) {
+        this.sales = sales;
+    }
+}
---
+++ Homework/2 SalesDatabase/HibernateCodeFirstHomework/src/main/java/relations/StoreLocation.java
@@ -0,0 +1,36 @@
+package relations;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "stores_location")
+public class StoreLocation extends BaseEntity{
+    private String locationName;
+    private Set<Sale> sales;
+
+    public StoreLocation() {
+    }
+
+    @Column(name = "location_name")
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+
+    @OneToMany(mappedBy = "storeLocation")
+    public Set<Sale> getSales() {
+        return sales;
+    }
+
+    public void setSales(Set<Sale> sales) {
+        this.sales = sales;
+    }
+}
